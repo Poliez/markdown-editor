3a7cef043c14e64d6d90ef8da142674322a8a597
Paolo Anastagi <poliez.poliez.p@gmail.com>
2018-09-05 00:35:46 +0200

    feat(db): add database context manager

---
+++ php/util/dbManager.php
@@ -0,0 +1,55 @@
+<?php  
+	require "dbConfig.php";
+    
+    $db = new MarkdownDbManager();
+
+	class MarkdownDbManager {
+		private $mysqli_conn = null;
+	
+		function MarkdownDbManager(){
+			$this->openConnection();
+		}
+    
+    	function openConnection(){
+    		if (!$this->isOpened()){
+    			global $dbHostname;
+    			global $dbUsername;
+    			global $dbPassword;
+    			global $dbName;
+    			
+    			$this->mysqli_conn = new mysqli($dbHostname, $dbUsername, $dbPassword);
+				if ($this->mysqli_conn->connect_error) 
+					die('Connect Error (' . $this->mysqli_conn->connect_errno . ') ' . $this->mysqli_conn->connect_error);
+
+				$this->mysqli_conn->select_db($dbName) or
+					die ('Can\'t select $dbName: ' . mysqli_error());
+			}
+    	}
+    
+    	function isOpened(){
+       		return ($this->mysqli_conn != null);
+    	}
+
+		function performQuery($queryText) {
+			if (!$this->isOpened())
+				$this->openConnection();
+			
+			return $this->mysqli_conn->query($queryText);
+		}
+		
+		function sqlInjectionFilter($parameter){
+			if(!$this->isOpened())
+				$this->openConnection();
+				
+			return $this->mysqli_conn->real_escape_string($parameter);
+		}
+
+		function closeConnection(){
+ 	       	if($this->mysqli_conn !== null)
+				$this->mysqli_conn->close();
+			
+			$this->mysqli_conn = null;
+		}
+	}
+
+?>
