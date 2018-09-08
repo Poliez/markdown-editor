2b2df3e92d63b12fc2a2a111492f8704b7885a23
Paolo Anastagi <poliez.poliez.p@gmail.com>
2018-09-08 23:52:07 +0200

    refactor(editor): clean editor code  and add aside toolbar

--- php/editor.php
+++ php/editor.php
@@ -1,73 +1,30 @@
-<?php
-	session_start();
-	require_once "./util/markdownDb.php";
-?>
-
 <!doctype html>
 <html lang="it-IT">
 <head>
 	<title>Markdown Editor</title>
-	<link rel="stylesheet" href="../css/files.css" />
+	<link rel="stylesheet" href="../css/editor.css" />
+    <script type="text/javascript" src="./../js/explorer.js"></script>
 </head>
-<body>
-
-    <script>
-        function showHideElem(id){
-            let elem = document.getElementById(id);
-
-            if(elem.style.display == "none"){
-                elem.style.display = "block";
-                return;
-            }
-
-            elem.style.display = "none";
-        }
-    </script>
-
-	<div id="docs">
-    <h1>Documenti</h1>
-    <div class="explorer">
+<body class="flex-container">
+	<aside class="editor-element">
+    <div class="explorer-operations">
+        <img class="operation" onclick="newFolder()"    src="./../images/create-new-folder.svg"/>
+        <img class="operation" onclick="newNote()"      src="./../images/add-note.svg" />
+        <!--<img class="operation" onclick="editName()"     src="./../images/edit.svg" />-->
+        <img class="operation" onclick="deleteNote()"   src="./../images/delete.svg" />
+    </div>
+    <div id="explorer">
         <?php
-            $folders = getFoldersWithDocuments($_SESSION["userId"]);
-
-            $nfolders = count($folders);
-
-            for($i = 0; $i < $nfolders; $i++) {
-                
-                echo "<div class='folder'>";
-
-                    printTd(
-                            $folders[$i]->Name,
-                            $folders[$i]->Id,
-                            "dir",
-                            "showHideElem(\"div$i\")"
-                        );
-                    
-                    echo "<div id='div$i' class='folder-content'>";
-
-                    $docs = $folders[$i]->Documents;
-                    $ndocs = count($docs);
-                    for($j = 0; $j < $ndocs; $j++)
-                        printTd(
-                            $docs[$j]->Name,
-                            $docs[$j]->Id,
-                            "file",
-                            null
-                        );
-
-                    echo "</div>";
-                echo "</div>";
-            }
-
-            function printTd($name, $namehref, $class, $onclick){
-                print("
-                    <div class='$class' onclick='$onclick'>
-                        $name
-                    </div>");
-            }
+            require "explorer.php"
         ?>
-
     </div>
 
+    </aside>
+
+    <textarea class="editor-element writing"></textarea>
+
+    <div id="converted-container" class="editor-element writing markdown">
+        
+    </div>
 </body>
 </html>
